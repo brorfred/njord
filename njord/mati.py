ba1fd5038c40b79d12ac6c3ca6b5725796144bbc
Bror Jonsson <brorfred@gmail.com>
2014-08-06 15:54:45 -0400

    Fixed up mati.py

--- njord/mati.py
+++ njord/mati.py
@@ -1,52 +1,47 @@
+import os
 from datetime import datetime as dtm
+import urllib
 
 import numpy as np
 import pylab as pl
-from scipy.spatial import KDTree, cKDTree
-import matplotlib.cm as cm
-from matplotlib.colors import LogNorm, Normalize
 
-import pycdf
+import numpy as np
+
 from pyhdf.SD import SD,SDC
+import base
 
-import projmaps
-import gmtgrid
-import figpref
-from hitta import GBRY
+class Cal(base.Grid):
 
-class Cal:
+    def __init__(self, **kwargs):
+        super(Cal, self).__init__(**kwargs)
+        self.add_mp()
 
-    def __init__(self, datadir="/projData/sat/mati/cal/",ijarea=[],
-                 lat1=None,lat2=None,lon1=None,lon2=None):
-        self.i1 = 0
-        self.i2 = 3405
-        self.j1 = 0
-        self.j2 = 3840
-        self.datadir = datadir
-        g = SD(datadir + 'cal_aco_3840_Latitude_Longitude.hdf', SDC.READ)
+    def setup_grid(self):
+        """Setup necessary variables for grid """
+
+        if not os.path.isfile(self.datadir + self.gridfile):
+            urllib.urlretrieve(self.dataurl + self.gridfile,
+                               self.datadir + self.gridfile)
+        g = SD(self.datadir + self.gridfile, SDC.READ)
         self.llat = g.select('Latitude')[:]
         self.llon = g.select('Longitude')[:]
         
-    def load(self,fldname,jd=0,yr=0,mn=1,dy=1):
+    def load(self, fldname, **kwargs):
         """ Load Cali Current fields for a given day"""
-        i1=self.i1; i2=self.i2; j1=self.j1; j2=self.j2
-        if jd!=0:
-            yr = pl.num2date(jd).year
-            mn = pl.num2date(jd).month
-            dy = pl.num2date(jd).day
-        elif yr!=0:
-            jd = pl.date2num(dtm(yr,mn,dy))
-        md = jd - pl.date2num(dtm(1992,10,05))
-        yd = jd - pl.date2num(dtm(yr,1,1)) + 1
+        self._timeparams(**kwargs)
         
         if fldname == 'chl':
-            filename = "/C%04i%03i_chl_mapped.hdf" % (yr,yd)
+            filename = "/C%04i%03i_chl_mapped.hdf" % (self.yr, self.yd)
             #ncfieldname = 'chl_%04i_%03i' % (yr,yd)
             def scale(PV): return 10**(PV*0.015-2)
         elif fldname == 'sst':
-            filename = "/M%04i%03i_sst_mapped.hdf" % (yr,yd)
+            filename = "/M%04i%03i_sst_mapped.hdf" % (self.yr, self.yd)
             #ncfieldname = 'sst_%04i_%03i' % (yr,yd)            
             def scale(PV): return PV*0.15000001-3
+        if not os.path.isfile(self.datadir + filename):
+            print "Downloading " + filename
+            self.download(fldname, self.jd)
+            
         h = SD(self.datadir + filename,SDC.READ)        
         ncfieldname = h.datasets().keys()[0]
         fld =  h.select(ncfieldname)
@@ -57,78 +52,27 @@ class Cal:
         PV[PV==255] = np.nan
         self.__dict__[fldname] = scale(PV)
 
-        
-    def add_ij(self):
-        i1=self.i1; i2=self.i2;j1=self.j1; j2=self.j2
-        self.jmat,self.imat = np.meshgrid(np.arange(self.j2-self.j1),
-                                          np.arange(self.i2-self.i1))
-        self.ijvec = np.vstack((np.ravel(self.imat),np.ravel(self.jmat))).T
-    def add_kd(self):
-        self.kd = cKDTree(list(np.vstack((np.ravel(self.llon),
-                                          np.ravel(self.llat))).T))
-    def ll2ij(self,lon,lat,nei=1):
-        if not hasattr(self,'kd'):
-            self.add_kd()
-            self.add_ij()
-        dist,ij = self.kd.query(list(np.vstack((lon,lat)).T),nei)
-        return self.ijvec[ij-1][:,0],self.ijvec[ij-1][:,1]
-    """
-    def create_ijvecs(self):
-        if not hasattr(self,'ns'):
-            self.init_nasa()
-        ns = self.ns
-        kd = cKDTree(list(np.vstack((np.ravel(ns.lon),np.ravel(ns.lat))).T))
-        dist,ij = kd.query(list(np.vstack((np.ravel(self.llon),
-                                           np.ravel(self.llat))).T))
-        l3j,l3i = np.meshgrid(np.arange(ns.j2-ns.j1),
-                              np.arange(ns.i2-ns.i1))
-        l3ij = np.vstack((np.ravel(l3i),np.ravel(l3j))).T
-        gomj,gomi = np.meshgrid(np.arange(j2-j1),np.arange(i2-i1))
-        #gomij = np.array(zip(np.ravel(gomi),np.ravel(gomj)))
-        gomij = np.vstack((np.ravel(gomi),np.ravel(gomj))).T
-        def gla(kk):
-            mat = self.imat*0
-            mat[self.ijvec[:,0],self.ijvec[:,1]] = l3ij[ij,:][:,kk]
-            #for i,j in gomij:
-            #    mat[i,j] = l3ij[ij,:][n,kk]
-            #    n+=1
-            return mat
-        self.si = gla(1)
-        self.satj = np.ravel(gla(0))
-        self.sati = np.ravel(gla(1))
-        
-    def create_satijvecs(self):
-        i1=self.i1; i2=self.i2;j1=self.j1; j2=self.j2
-        if not hasattr(self,'ns'):
-            self.init_nasa()
-        ns = self.ns
-        kd = cKDTree(list(np.vstack((np.ravel(self.llon),
-                                     np.ravel(self.llat))).T))
-        dist,ij = kd.query(list(np.vstack((np.ravel(ns.lon),
-                                           np.ravel(ns.lat))).T))
-        gomj,gomi = np.meshgrid(np.arange(ns.j2-ns.j1),
-                                np.arange(ns.i2-ns.i1))
-        l3j,l3i = np.meshgrid(np.arange(j2-j1),np.arange(i2-i1))
-        l3ij  = np.vstack((np.ravel(l3i),np.ravel(l3j))).T
-        gomij = np.vstack((np.ravel(gomi),np.ravel(gomj))).T
-        def gla(kk):
-            mat = gomj[:]*0
-            mat[gomij[:,0],gomij[:,1]] = l3ij[ij,:][:,kk]
-            return mat
-        self.gcmj = np.ravel(gomi)
-        self.gcmi = np.ravel(gomj)
-        self.si = gla(1)
-        self.satj = np.ravel(gla(0))
-        self.sati = np.ravel(gla(1))
-    """
-        
-    def add_landmask(self):
-        """
-        from scipy.stats import nanmean
-        nc = pycdf.CDF(self.datadir + '/oscar_vel2005.nc')
-        u = nc.var('u')[:,:,:,:1081]
-        msk = np.squeeze(nanmean(u,axis=0))
-        msk = msk*0
-        msk[np.isnan(msk)]=1
-        self.landmask = gmtgrid.convert(msk,self.gr)
-        """
+
+    def download(self, fldname, jd):
+        self._timeparams(jd=jd)
+        if fldname == "chl":
+            url = "%s/%04i/C%04i_chl_day/" % (self.dataurl, self.yr, self.yr)
+            filename = "/C%04i%03i_chl_mapped.hdf" % (self.yr, self.yd)
+        elif fldname == "sst":
+            url = "%s/%04i/M%04i_sst_day/" % (self.dataurl, self.yr, self.yr)
+            filename = "/M%04i%03i_sst_mapped.hdf" % (self.yr, self.yd)
+        else:
+            raise ValueError("Wrong fieldname")
+        urllib.urlretrieve(url+filename, self.datadir + filename)
+
+    @property
+    def landmask(self):
+        if not hasattr(self, "_landmask"):
+            filename = os.path.basename(self.landurl)
+            if not os.path.isfile(self.datadir + filename):
+                urllib.urlretrieve(self.dataurl + self.landurl,
+                                   self.datadir + filename)
+            h = SD(self.datadir + filename, SDC.READ)        
+            ncfieldname = h.datasets().keys()[0]
+            self._landmask =   h.select(ncfieldname)[:] == -1
+        return self._landmask
