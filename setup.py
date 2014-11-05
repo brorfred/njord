6e5f1be7caf63daf34b22157a0627e478f3dfac3
Bror Jonsson <brorfred@gmail.com>
2014-11-05 10:21:50 -0500

    Added download_url to setup.py

--- setup.py
+++ setup.py
@@ -3,25 +3,21 @@
 usage:    python setup.py sdist
           python setup.py install
 """
-
-
-from distutils.core import setup
+from setuptools import setup, find_packages
 
 setup(name = 'njord',
-      version = '0.5.9.1',
+      version = '0.5.9.5',
       description = 'Package to simplify working with gridded data',
       long_description = "README.md",
       #long_description=open('docs/README.rst', 'rt').read()
-
       author = 'Bror Jonsson',
       author_email = 'brorfred@gmail.com',
       url = 'http://brorfred.org/python_dists/njord/',
-      requires = ["numpy(>=1.5)",
-                  "matplotlib(>=1.1.0)",
-                  "mpl_toolkits(>=1.0)",
-                  "requests(>=1.1.0)",
-                  "projmap(>=0.5)",
-                  "requests(>=1.1)"],
+      download_url='https://github.com/brorfred/njord/tarball/0.5',
+      install_requires = ["numpy>=1.5",
+                          "matplotlib>=1.1.0",
+                          "requests>=1.1.0",
+                          "projmap>=0.5",],
       packages = ['njord'],
       scripts = ["njord/njordstatus",],
       package_data = {'njord': ['njord.cfg']},
