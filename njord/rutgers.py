106dd4acc8fba26626b262a950366d8392e2e83a
Bror Jonsson <brorfred@gmail.com>
2015-04-03 15:56:00 -0400

    Added landmask and download to rutgers

--- njord/rutgers.py
+++ njord/rutgers.py
@@ -1,4 +1,4 @@
-import os, urllib2
+import os, urllib2, urlparse
 from datetime import datetime as dtm
 import math
 
@@ -13,12 +13,15 @@ class Rutgers(base.Grid):
     """ Baseclass for all rutgers projects """
     def __init__(self, **kwargs):
         super(Rutgers, self).__init__(**kwargs)
-        
-    def add_landmask(self):
-        """ Add a landmask attribute """
-        g = netcdf_file(self.gridfile)
-        self.landmask = g.variables['mask_rho'][:]
 
+    @property
+    def landmask(self):
+        """ Return attribute """
+        if not hasattr(self, "_landmask"):
+            with netcdf_file(self.gridfile) as gH:
+                self._landmask = gH.variables['mask_rho'][:]
+        return self._landmask
+            
     def add_utmxy(self):
         """Add catersian coordinates """
         g = netcdf_file(self.gridfile)
@@ -27,40 +30,46 @@ class Rutgers(base.Grid):
 
     def setup_grid(self):
         """Setup necessary variables for grid """
-        g = netcdf_file(self.gridfile, 'r')
-        self.llat = g.variables['lat_rho'][:]
-        self.llon = g.variables['lon_rho'][:]-360
-        self.depth = g.variables['h'][:]
-        self.Cs_r = g.variables['Cs_r'][:]
+        if not os.path.isfile(self.gridfile):
+            url = urlparse.urljoin(self.gridurl, os.path.basename(self.gridfile))
+            self.retrive_file(url, self.gridfile)
+        with netcdf_file(self.gridfile) as gH:
+            self.llat  = gH.variables['lat_rho'][:]
+            self.llon  = gH.variables['lon_rho'][:]-360
+            self.depth = gH.variables['h'][:]
+            self.Cs_r  = gH.variables['Cs_r'][:]
 
-    def load(self,fldname,jd=None,yr=0,mn=1,dy=1,hr=3):
+    def load(self,fldname, **kwargs):
         """ Load velocity fields for a given day"""
+        self._timeparams(**kwargs)
         if fldname == "uv":
             self.load('u',jd=jd, yr=yr, mn=mn, dy=dy, hr=hr)
             self.load('v',jd=jd, yr=yr, mn=mn, dy=dy, hr=hr)
             self.uv = np.sqrt(self.u[:,1:,:]**2 + self.v[:,:,1:]**2)
             return
         
-        i1=self.i1; i2=self.i2; j1=self.j1; j2=self.j2
-        if yr!=0:
-            jd = pl.date2num(dtm(yr,mn,dy,hr))
-        nc = netcdf_file(self.jd2filename(jd))
-        fld =  np.squeeze(nc.variables[fldname][:]).copy()
-        fld[fld>9999] = np.nan
-        self.__dict__[fldname] = fld
-        self.ssh =  np.squeeze(nc.variables['zeta'][:])
-        self.zlev = ((self.depth + self.ssh)[np.newaxis,:,:] *
-                     self.Cs_r[:,np.newaxis,np.newaxis])
+        filename = self.jd2filename(self.jd)
+        if not os.path.isfile(filename):
+            url = urlparse.urljoin(self.dataurl, os.path.basename(filename))
+            print url
+            print filename
+            self.retrive_file(url, filename)
+        with netcdf_file(filename) as nc:
+            fld =  np.squeeze(nc.variables[fldname][:]).copy()
+            fld[fld>9999] = np.nan
+            self.__dict__[fldname] = fld
+            self.ssh =  np.squeeze(nc.variables['zeta'][:])
+            self.zlev = ((self.depth + self.ssh)[np.newaxis,:,:] *
+                          self.Cs_r[:,np.newaxis,np.newaxis])
 
 class NWA(Rutgers):
     """Setup North West Atlantic"""
     def __init__(self, **kwargs):
         super(NWA, self).__init__(**kwargs)
 
-    def jd2filename(self,jd):
-        if jd == None: jd = 730217 
-        filename = "/nwa_avg_%05i.nc" % (jd - 714782)
-        return "%s/%s/%s" % (self.datadir, pl.num2date(jd).year, filename)    
+    def jd2filename(self, jd):
+        filename = "nwa_avg_%05i.nc" % (jd - 714782)
+        return os.path.join(self.datadir, filename)    
 
     def download(self, filename):
         """Download a missing file from Rutger's website"""
