139648d068e21b5e3876278670312e26c7334ffb
Bror Jonsson <brorfred@gmail.com>
2018-08-19 11:14:59 -0400

    Raise error if used with python2

--- njord/__init__.py
+++ njord/__init__.py
@@ -1,2 +1,16 @@
 
 
+import os 
+import six
+
+if six.PY2:
+    raise ImportError("This version is not working wih python2 yet")
+
+
+
+__all__ = ["ghrsst", "mimoc", "smos", "globcolour", "mldclim", 
+           "avhrr", "gmtgrid", "nasa", "strang", "gompom", "fvcom", 
+           "bem", "hycom", "ncep", "topaz", "bgccsm", "jpl", 
+           "cesm", "landmask", "winds", "oscar", "woa", "ecco", "mati", 
+           "poseidon", "worldclim", "gebco", "rutgers"]
+
